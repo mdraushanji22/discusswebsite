6374eb7c4b76c28ecbe75f0d76d2aa14f41f30c2
mdraushan <mdraushanji22@gmail.com>
2026-02-04 22:43:00 +0530

    discuss-website

--- client/ask.php
+++ client/ask.php
@@ -6,6 +6,19 @@
             <input type="text" name="title" class="form-control" id="title"
                 placeholder="Enter Question">
         </div>
+        <div class="col-6 offset-sm-3 margin-bottom-15">
+            <label for="description" class="form-label">Description</label>
+            <textarea name="description" class="form-control" id="description"
+                placeholder="Enter Description"></textarea>
+        </div>
+        <div class="col-6 offset-sm-3 margin-bottom-15">
+            <label for="category" class="form-label">Category</label>
+            <select name="category" class="form-control" id=" category">
+                <option value="">Mobile</option>
+                <option value="">General</option>
+                <option value="">Coding</option>
+            </select>
+        </div>
 
         <div class="col-6 offset-sm-3 margin-bottom-15">
             <button type="submit" name="signup" class="btn btn-primary">Submit</button>
